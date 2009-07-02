e19b56351a9b6cd1fe10ab9b01af1c39ab87bfa6
pi.two.kay <pi.two.kay@feef7bb0-5087-11de-b90f-197667375a34>
2009-07-02 15:14:18 +0000

    Wrote a distutils script for easy installation.

---
+++ branches/pycow/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='PyCow',
+	version='0.3',
+	description='Python to JavaScript translator',
+	author='p2k',
+	author_email='patrick.p2k.schneider@gmail.com',
+	url='http://code.google.com/p/pygowave-server/',
+	packages=['pycow'],
+)
